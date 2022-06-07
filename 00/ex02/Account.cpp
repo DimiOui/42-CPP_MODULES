2612bdaa8a489fedf02698645570559774c6c5f6
Dimi <omygoditsdimi@gmail.com>
2022-06-07 11:33:56 +0200

    getting the grasp of iterators--ex02

---
+++ 00/ex02/Account.cpp
@@ -0,0 +1,45 @@
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::~Account()
+{
+}
+
+int		Account::getNbAccounts()
+{
+}
+
+int		Account::getTotalAmount()
+{
+}
+
+int		Account::getNbDeposits()
+{
+}
+
+int		Account::getNbWithdrawals()
+{
+}
+
+void	Account::displayAccountsInfos()
+{
+}
+
+void	Account::makeDeposit(int deposit)
+{
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+}
+
+int		Account::checkAmount() const
+{
+}
+
+void	Account::displayStatus() const
+{
+}
