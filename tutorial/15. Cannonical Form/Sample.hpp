e3526451da7a99aef68261e696129045e1b4aca9
DimiOui <omygoditsdimi@gmail.com>
2022-06-14 13:01:01 +0200

    cannonical

--- tutorial/15. Cannonical Form/Sample.hpp
+++ tutorial/15. Cannonical Form/Sample.hpp
@@ -1,19 +1,25 @@
 #ifndef SAMPLE_HPP
 #define SAMPLE_HPP
 
+#include <iostream>
+
 #pragma once
 
 class Sample
 {
 public:
-    Sample();
-    Sample(Sample const &obj);
-    virtual ~Sample();
+    Sample();                               //CANONICAL
+    Sample(Sample const &obj);              //CANONICAL
+    virtual ~Sample();                      //CANONICAL
 
-    Sample  &operator=(Sample const &rhs);
+    Sample  &operator=(Sample const &rhs);  //CANONICAL
+    int     getFoo() const;
+
 private:
-
+    int     _foo;
 };
+
+std::ostream    &operator<<(std::ostream &o, Sample const &i);
 
 #endif
 
