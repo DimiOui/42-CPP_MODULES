13938474fef91310879ec85d1b1e756940a55d01
DimiOui <omygoditsdimi@gmail.com>
2022-08-29 16:42:57 +0200

    fixed the sort in shortestSpan

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -1,6 +1,12 @@
 #include "Span.hpp"
 
-Span::Span(unsigned int const &size) : _size(size)
+Span::Span() : _size(0)
+{
+	std::cout << KYEL "Default (empty) Span class constructor called." << std::endl;
+	return;
+}
+
+Span::Span(unsigned int size) : _size(size)
 {
 	std::cout << KYEL "Sized Span class constructor called." << std::endl;
 	return;
@@ -23,15 +29,13 @@ Span &Span::operator=(Span const &obj)
 	std::cout << KYEL "Assignement operator overload for Span class called." << std::endl;
 	if (this != &obj)
 	{
-		//this->_size = obj.getSize();
-		//this->_numbersVect = obj.getNumbers();
-		this->_size = obj._size;
-		this->_numbersVect = obj._numbersVect;
+		this->_size = obj.getSize();
+		this->_numbersVect = obj.getNumbers();
 	}
 	return (*this);
 }
 
-void	Span::addNumber(unsigned int &n)
+void	Span::addNumber(unsigned int n)
 {
 	if (this->_size == _numbersVect.size())
 		throw std::out_of_range(KRED "Vector is full, can't add more numbers.");
@@ -39,33 +43,27 @@ void	Span::addNumber(unsigned int &n)
 	std::cout << KWHT "Number " << n << " added to vector." << std::endl;
 }
 
-void	Span::addByItRange(std::vector<int>::const_iterator begin, std::vector<int>::const_iterator end)
+void	Span::addByItRange(std::vector<int>::iterator begin, std::vector<int>::iterator end)
 {
-	std::vector<int> tmp = std::vector<int>(begin, end);
-	//std::vector<int>	tmp(begin, end);
+	std::vector<int>	tmp(begin, end);
+
 	if (tmp.size() + _numbersVect.size() > _size)
 		throw std::length_error(KRED "Vector is full, can't add more numbers.");
 	_numbersVect.insert(_numbersVect.end(), begin, end);
 }
 
-void	Span::printVect() const
-{
-	std::cout << " pr" << std::endl;
-	for (std::vector<int>::const_iterator i(_numbersVect.begin()); i != _numbersVect.end(); ++i)
-		std::cout << " " << *i << std::endl;
-}
-
 unsigned int	Span::shortestSpan() const
 {
+	std::vector<int>	tmp = _numbersVect;
+
 	if (_numbersVect.size() < 2)
 		throw std::logic_error(KRED "Not enough numbers, shortest distance not calculated.");
-
-	std::sort(_numbersVect.begin(), _numbersVect.end());
-	unsigned int	short_span = std::abs(_numbersVect[0] - _numbersVect[1]);
-	for (unsigned int i = 0; i != _numbersVect.size() - 1; ++i)
+	std::sort(tmp.begin(), tmp.end());
+	unsigned int	short_span = std::abs(tmp[0] - tmp[1]);
+	for (unsigned int i = 0; i != tmp.size() - 1; ++i)
 	{
-		if (_numbersVect[i + 1] - _numbersVect[i] < static_cast<int>(short_span))
-			short_span = _numbersVect[i + 1] - _numbersVect[i];
+		if (tmp[i + 1] - tmp[i] < static_cast<int>(short_span))
+			short_span = tmp[i + 1] - tmp[i];
 	}
 	return (short_span);
 }
--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -1,247 +1,82 @@
-//#include "Span.hpp"
-
-//int	randSpan()
-//{
-//	srand(static_cast<unsigned int>(clock()));
-//	return (rand() % ((10000) + 1));
-//}
-
-//int	main()
-//{
-//	{
-//		Span sp = Span(5);
-
-//		sp.addNumber(6);
-//		sp.addNumber(3);
-//		sp.addNumber(17);
-//		sp.addNumber(9);
-//		sp.addNumber(11);
-
-//		std::cout << sp.shortestSpan() << std::endl;
-//		std::cout << sp.longestSpan() << std::endl;
-
-//		return 0;
-//	}
-//	{
-//		Span	sp2 = Span(50);
-//		std::vector<int> randNumbers(42, 0);
-//		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
-//		sp2.addByItRange(randNumbers.begin(), randNumbers.end());
-//		try
-//		{
-//			std::cout << sp2 << std::endl;
-//			sp2.addNumber(1);
-//			sp2.addNumber(4);
-//			std::cout << sp2 << std::endl;
-//		}
-//		catch(const std::exception& e)
-//		{
-//			std::cerr << e.what() << '\n';
-//		}
-//	}
-//	{
-//		Span	sp3 = Span(10000);
-//		std::vector<int> randNumbers(10000, 0);
-//		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
-//		sp3.addByItRange(randNumbers.begin(), randNumbers.end());
-//		try
-//		{
-//			std::cout << sp3 << std::endl;
-//		}
-//		catch(const std::exception& e)
-//		{
-//			std::cerr << e.what() << '\n';
-//		}
-//	}
-//	{
-//		Span	sp4 = Span(2);
-//		try
-//		{
-//			std::cout << sp4 << std::endl;
-//		}
-//		catch(const std::exception& e)
-//		{
-//			std::cerr << e.what() << '\n';
-//		}
-//	}
-//	{
-//		Span	sp5 = Span(42);
-//		std::vector<int> randNumbers(42, 0);
-//		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
-//		sp5.addByItRange(randNumbers.begin(), randNumbers.end());
-//		try
-//		{
-//			sp5.addNumber(5);
-//		}
-//		catch(const std::exception& e)
-//		{
-//			std::cerr << e.what() << '\n';
-//		}
-//	}
-//	return 0;
-//}
-
 #include "Span.hpp"
 
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include <iterator>
-#include <unistd.h>
-#include <vector>
-
-static unsigned long mix(unsigned long a, unsigned long b, unsigned long c) {
-    a = a - b;
-    a = a - c;
-    a = a ^ (c >> 13);
-    b = b - c;
-    b = b - a;
-    b = b ^ (a << 8);
-    c = c - a;
-    c = c - b;
-    c = c ^ (b >> 13);
-    a = a - b;
-    a = a - c;
-    a = a ^ (c >> 12);
-    b = b - c;
-    b = b - a;
-    b = b ^ (a << 16);
-    c = c - a;
-    c = c - b;
-    c = c ^ (b >> 5);
-    a = a - b;
-    a = a - c;
-    a = a ^ (c >> 3);
-    b = b - c;
-    b = b - a;
-    b = b ^ (a << 10);
-    c = c - a;
-    c = c - b;
-    c = c ^ (b >> 15);
-    return c;
+int	randSpan()
+{
+	srand(static_cast<unsigned int>(clock()));
+	return (rand() % ((10000) + 1));
 }
 
-static int random_number() {
-    srand(mix(clock(), time(NULL), getpid()));
-    return rand();
-}
+int	main()
+{
+	{
+		Span sp = Span(5);
 
-int main(int ac, char** av) {
+		sp.addNumber(0);
+		sp.addNumber(10);
+		sp.addNumber(26);
+		sp.addNumber(39);
+		sp.addNumber(11);
 
-    std::cout << KMAG;
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
 
-    std::cout << KMAG "====================== [ TEST ] Iterators ====================" << std::endl
-              << std::endl;
-
-    bool verbose = false;
-    if (ac > 1 and not(std::string(av[1]).compare("-v"))) {
-        verbose = true;
-        std::cout << "Verbose mode: [ON]" << std::endl;
-    } else
-        std::cout << "Verbose mode: [OFF]" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << KMAG "========== Trying longestSpan() on an empty vector: ==========" << std::endl
-              << std::endl;
-
-    Span span = Span(5);
-    try {
-        std::cout << " longestSpan(): " << span.longestSpan() << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl
-              << std::endl;
-    ;
-
-    std::cout << KMAG "========== Try to add too many values to a vector: ===========" << std::endl
-              << std::endl;
-    try {
-        span.addNumber(random_number());
-        span.addNumber(random_number());
-        span.addNumber(random_number());
-        span.addNumber(random_number());
-        span.addNumber(random_number());
-        span.addNumber(random_number());
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl
-              << std::endl;
-    ;
-
-    std::cout << KMAG "==================== Call longestSpan() ======================" << std::endl
-              << std::endl;
-
-    if (verbose)
-        span.printVect();
-
-    std::cout << std::endl;
-
-    try {
-        std::cout << " longestSpan(): " << span.longestSpan() << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl
-              << std::endl;
-
-    std::cout << KMAG "==================== Call shortestSpan() =====================" << std::endl
-              << std::endl;
-
-    if (verbose)
-        span.printVect();
-
-    std::cout << std::endl;
-
-    try {
-        std::cout << " shortestSpan(): " << span.shortestSpan() << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl
-              << std::endl;
-
-    std::cout << KMAG "=============== Call to overloaded addNumber() ===============" << std::endl
-              << std::endl;
-
-    Span fat_span = Span(15000);
-    std::vector<int> init_vec(15000);
-    std::generate(init_vec.begin(), init_vec.end(), random_number);
-    try {
-        fat_span.addNumber(init_vec.begin(), init_vec.end());
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl;
-
-    try {
-        std::cout << KMAG " shortestSpan(): " << fat_span.shortestSpan() << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    try {
-        std::cout << KMAG " longestSpan(): " << fat_span.longestSpan() << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl
-              << std::endl;
-
-    std::cout << KMAG "== Try to add too many values to a vector with addNumber(): ==" << std::endl
-              << std::endl;
-
-    try {
-        fat_span.addNumber(init_vec.begin(), init_vec.end());
-    } catch (std::exception& e) {
-        std::cerr << KRED << e.what() << KNRM << std::endl;
-    }
-    std::cout << std::endl;
-
-    std::cout << KNRM;
-
-    return EXIT_SUCCESS;
+		//return 0;
+	}
+	{
+		Span	sp2 = Span(50);
+		std::vector<int> randNumbers(42, 0);
+		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
+		sp2.addByItRange(randNumbers.begin(), randNumbers.end());
+		try
+		{
+			std::cout << sp2 << std::endl;
+			sp2.addNumber(1);
+			sp2.addNumber(4);
+			std::cout << sp2 << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	{
+		Span	sp3 = Span(10000);
+		std::vector<int> randNumbers(10000, 0);
+		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
+		sp3.addByItRange(randNumbers.begin(), randNumbers.end());
+		try
+		{
+			std::cout << sp3 << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	{
+		Span	sp4 = Span(2);
+		try
+		{
+			std::cout << sp4 << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	{
+		Span	sp5 = Span(42);
+		std::vector<int> randNumbers(42, 0);
+		std::generate(randNumbers.begin(), randNumbers.end(), randSpan);
+		sp5.addByItRange(randNumbers.begin(), randNumbers.end());
+		try
+		{
+			sp5.addNumber(5);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	return 0;
 }
